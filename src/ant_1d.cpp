53581ce26e85a881ca12493bc0f5618d87bdfd30
nppmatt <nppmatt@gmail.com>
2024-05-05 12:59:38 -0400

    full 1d grid probe

--- src/ant_1d.cpp
+++ src/ant_1d.cpp
@@ -6,7 +6,7 @@
 
 int main() {
 	const unsigned int cellCount = 2 << FINE;
-	const double imp0 = 377.0;
+	const double fsImp = 376.73031367;
 
 	Eigen::VectorXd E_z(cellCount);
 	Eigen::VectorXd H_y(cellCount);
@@ -16,15 +16,15 @@ int main() {
 	const unsigned int maxTime = 10000;
 	for (unsigned int qTime = 0; qTime < maxTime; ++qTime) {
 		for (unsigned int mm = 0; mm < cellCount-1; ++mm) {
-			H_y(mm) = H_y(mm) + (E_z(mm+1) - E_z(mm)) / imp0;
+			H_y(mm) = H_y(mm) + (E_z(mm+1) - E_z(mm)) / fsImp;
 		}
 
 		for (unsigned int mm = 1; mm < cellCount; ++mm) {
-			E_z(mm) = E_z(mm) + (H_y(mm) - H_y(mm-1)) * imp0;
+			E_z(mm) = E_z(mm) + (H_y(mm) - H_y(mm-1)) * fsImp;
 		}
 
 		E_z(0) = exp( -(qTime - 30.0) * (qTime - 30.0) / 100.0 );
-		std::cout << E_z(50) << std::endl;
+		std::cout << E_z << std::endl;
 	}
 
 	return 0;

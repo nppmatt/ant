47db1f736a0e1b74441460aba96e6072a3dc2f12
nppmatt <nppmatt@gmail.com>
2024-05-05 16:43:01 -0400

    removed extraneous video frames & added ABC

--- process-data.sh
+++ process-data.sh
@@ -4,10 +4,12 @@ echo "$(date) Job Selection" >> joblog
 ./gen-data.sh
 
 echo "$(date) Generating data plots." >> joblog
+rm plot-export/*
 ./gen-plot.py
 echo "$(date) Finished plotting." >> joblog
 
 echo "$(date) Generating videos." >> joblog
+rm video-export/*
 ./gen-video.sh
 echo "$(date) Finished videos." >> joblog
 

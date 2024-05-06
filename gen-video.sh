f6aa351dbf4513ce02da1fc2b0097d49028b2d29
nppmatt <nppmatt@gmail.com>
2024-05-06 04:35:17 -0400

    ffmpeg requires filenames with contiguous adjacent numbers

--- gen-video.sh
+++ gen-video.sh
@@ -4,17 +4,17 @@ PLOT_DIR=plot-export
 VIDEO_DIR=video-export
 
 echo "Generating 30fps video."
-ffmpeg -hide_banner -loglevel error -r 30 -i $PLOT_DIR/result_%6d.png -c:v libx264 -pix_fmt yuv420p -y $VIDEO_DIR/result30.mp4
+ffmpeg -hide_banner -loglevel error -i $PLOT_DIR/result_%5d.png -c:v libx264 -pix_fmt yuv420p -r 30 -y $VIDEO_DIR/result30.mp4
 
 echo "Generating 60fps video."
-ffmpeg -hide_banner -loglevel error -r 60 -i $PLOT_DIR/result_%6d.png -c:v libx264 -pix_fmt yuv420p -y $VIDEO_DIR/result60.mp4
+ffmpeg -hide_banner -loglevel error -i $PLOT_DIR/result_%5d.png -c:v libx264 -pix_fmt yuv420p -r 60 -y $VIDEO_DIR/result60.mp4
 
 echo "Generating 120fps video."
-ffmpeg -hide_banner -loglevel error -r 120 -i $PLOT_DIR/result_%6d.png -c:v libx264 -pix_fmt yuv420p -y $VIDEO_DIR/result120.mp4
+ffmpeg -hide_banner -loglevel error -i $PLOT_DIR/result_%5d.png -c:v libx264 -pix_fmt yuv420p -r 120 -y $VIDEO_DIR/result120.mp4
 
 echo "Generating 240fps video."
-ffmpeg -hide_banner -loglevel error -r 240 -i $PLOT_DIR/result_%6d.png -c:v libx264 -pix_fmt yuv420p -y $VIDEO_DIR/result240.mp4
+ffmpeg -hide_banner -loglevel error -i $PLOT_DIR/result_%5d.png -c:v libx264 -pix_fmt yuv420p -r 240 -y $VIDEO_DIR/result240.mp4
 
 echo "Generating 480fps video."
-ffmpeg -hide_banner -loglevel error -r 480 -i $PLOT_DIR/result_%6d.png -c:v libx264 -pix_fmt yuv420p -y $VIDEO_DIR/result480.mp4
+ffmpeg -hide_banner -loglevel error -i $PLOT_DIR/result_%5d.png -c:v libx264 -pix_fmt yuv420p -r 480 -y $VIDEO_DIR/result480.mp4
 

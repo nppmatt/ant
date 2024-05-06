29f6a7794c740d9776022faa390d1baf5ad567ea
nppmatt <nppmatt@gmail.com>
2024-05-05 22:51:02 -0400

    fix lerp scaling again

--- src/include/lerpTable.hpp
+++ src/include/lerpTable.hpp
@@ -15,7 +15,7 @@ double sin_table_0_0001_LERP(double x)
 {
     x = absd(x);
     x = modd(x, pi2);
-    double i = x * 1000.0;
+    double i = x * 10000.0;
     int index = (int)i;
     return lerp(i - index,        /* weight      */
         sintable_0_0001[index],     /* lower value */
@@ -27,7 +27,7 @@ double sin_table_0_00001_LERP(double x)
 {
     x = absd(x);
     x = modd(x, pi2);
-    double i = x * 10000.0;
+    double i = x * 100000.0;
     int index = (int)i;
     return lerp(i - index,        /* weight      */
         sintable_0_00001[index],     /* lower value */

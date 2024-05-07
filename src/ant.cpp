7dbc399ec381c0fcc7465f988896b46d820ba6a7
nppmatt <nppmatt@gmail.com>
2024-05-06 23:01:48 -0400

    16 elements lil wider

--- src/ant.cpp
+++ src/ant.cpp
@@ -65,7 +65,7 @@ int main() {
 
 	/* More simulation parameters. */
 	constexpr unsigned int src_x = 1022;
-	constexpr unsigned int arrayDelta = 22;
+	constexpr unsigned int arrayDelta = 44;
 
 	// center
 	constexpr unsigned int src7_y = 512;

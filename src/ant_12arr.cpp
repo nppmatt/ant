75123e0bdca82d6d85f2795f57c7836b515c40c5
nppmatt <nppmatt@gmail.com>
2024-05-06 23:26:59 -0400

    16 elem testing

--- src/ant_12arr.cpp
+++ src/ant_12arr.cpp
@@ -65,7 +65,7 @@ int main() {
 
 	/* More simulation parameters. */
 	constexpr unsigned int src_x = 1022;
-	constexpr unsigned int arrayDelta = 16;
+	constexpr unsigned int arrayDelta = 12;
 
 	// center
 	constexpr unsigned int src7_y = 512;
